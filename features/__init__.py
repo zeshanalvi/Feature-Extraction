00a3edc1bb55cb16678784999f4794a3be55c38b
Zeshan Khan <zeshankhanalvi@gmail.com>
2025-10-21 15:06:03 +0500

    Update __init__.py

--- features/__init__.py
+++ features/__init__.py
@@ -4,6 +4,6 @@
 #from . import image_read
 
 # features/__init__.py
-from .Lire import get_lires, color_correlogram, color_layout, compute_glcm, edge_histogram, extract_lire, tamura_features, jcd_descriptor, PHOG
+from .Lire import get_lires, color_correlogram, color_layout, compute_glcm, edge_histogram, extract_lire, tamura_features, jcd_descriptor, PHOG, extract_features_single
 from .LBP import get_lbps, get_lbp_single
 from .image_read import Dataset
